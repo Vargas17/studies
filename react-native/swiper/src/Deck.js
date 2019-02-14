27ddb5ec9b05bcc3c2f3261c7302d10094a996cc
Arthur <arthur@rethink.ws>
2019-02-14 18:24:51 -0200

    developing react-native swiper

--- react-native/swiper/src/Deck.js
+++ react-native/swiper/src/Deck.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
-import { View, Animated, Text } from 'react-native';
+import { View, Animated, PanResponder } from 'react-native';
 
 class Deck extends Component {
+  constructor(props) {
+    super(props);
+
+    const position = new Animated.ValueXY();
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        position.setValue({ x: gesture.dx, y: gesture.dy })
+      },
+      onPanResponderRelease: () => {}
+    });
+
+    this.panResponder = panResponder;
+    this.position = position;
+  }
+
   renderCards() {
-    return this.props.data.map(item => {
+    return this.props.data.map((item, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            style={this.position.getLayout()}
+            {...this.panResponder.panHandlers}
+          >
+            {this.props.renderCard(item)}
+          </Animated.View>
+        )
+      }
       return this.props.renderCard(item);
     });
   }
